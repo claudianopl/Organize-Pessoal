2c120e344e5d7f50bcef27892b5c4187c4806090
Claudiano Lima <claudianopereira047@gmail.com>
2020-07-01 11:50:38 -0300

    Foi adicionado um input com um maks mês/ano e o calendário foi alterado para ser apresentado apenas na /app, pois ficara mais organizado e mais limpo o código, além disso, foi adicionado um plugin jquery chamado mask.

--- public/assets/js/scriptAppHeader.js
+++ public/assets/js/scriptAppHeader.js
@@ -1,47 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-  /*
-  * 
-  * Função global para fazer requisições ajax e enviar as informações para o 
-  * servidor
-  */
+  /**
+   * Se comunica com o back-end através do ajax.
+   * A função envia os dados, para o back-end do laytou app, foi criada porque 
+   * é executada duas vezes, uma quando o usuário entra na página app e outra
+   * quando o usuário solicita uma nova data.
+   * @param {String} date 
+   */
   function getAjax(date) {
+    /*
     let url = window.location.pathname.split('/');
     const cauntUrl = url.length - 1;
     if(url[cauntUrl] == 'app') {
       url = '/app/dateApp';
     } 
-    else if(url[cauntUrl] == 'receitas') {
-      url = '/app/dateReceive';
-    } 
-    else if(url[cauntUrl] == 'despesas') {
-      /*
-      * Colocar a url como a rota para a requisição ajax
-      * url = '/routeAjaxApp
-      */
-      console.log('despesas');
-    } 
-    else if(url[cauntUrl] == 'tarefas') {
-      /*
-      * Colocar a url como a rota para a requisição ajax
-      * url = '/routeAjaxApp
-      */
-      console.log('tarefas');
-    }
-    else if(url[cauntUrl] == 'fixas') {
-      /*
-      * Colocar a url como a rota para a requisição ajax
-      * url = '/routeAjaxApp
-      */
-      console.log('fixas');
-    }
-    else if(url[cauntUrl] == 'carteiras') {
-      /*
-      * Colocar a url como a rota para a requisição ajax
-      * url = '/routeAjaxApp
-      */
-      console.log('carteiras');
-    }
-
+    o calendário só sera usado na página app,
+    então a url vai ser o back-end da página app
     
     $.ajax({
       type: 'post',
@@ -65,13 +38,16 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log(erro)
       }
     })
-    
+    */
   }
 
-  /*
-  * getDaysCalender(dateMonth, dateYear)
-  * Seleciona o mês e o ano atual, converte esses valores para string e apresenta
-  * esses valores ao usuário.
+  /**
+  * Formata o calendário para de int para string.
+  * A função seleciona o mês e o ano atual e formata os valores para string e 
+  * apresenta no calendário para o usuário.
+  * @param {Number} dateMonth 
+  * @param {Number} dateYear 
+  * @return {String}
   */
   function getDaysCalender(dateMonth, dateYear) {
     monthSelect = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun','Jul', 
@@ -86,49 +62,53 @@ document.addEventListener('DOMContentLoaded', function() {
       if(table[i].innerText === monthSelect[dateMonth]) {
         table[i].classList.add("tdActive");
       }
-    
     }
-
-    return `${dateMonth}-${dateYear}`;
+    return `${dateMonth}-${dateYear}`
   }
 
-  /*
-  * Selecionando o mês atual e o ano atual para serem apresentados ao usuário
-  */
-  let dt = new Date();
-  let month = dt.getMonth();
-  let year = dt.getFullYear();
+  /**
+   * Selecionando o mês atual e o ano atual para serem apresentados ao usuário.
+   */
+  const dt = new Date();
+  const month = dt.getMonth();
+  const year = dt.getFullYear();
+  /**
+   * Executa a função para formatar o calendário e retorna a data formata em string.
+   */
   getAjax(getDaysCalender(month, year));
+
   
 
-  /*
-  *  Selecionando o próximo ano ou o ano anterior
+
+  /**
+  * Seleciona o próximo ano, ou o ano anterior.
   */
   const btnPrev = document.querySelector(".ButtonPrevious");
   const btnNext = document.querySelector(".ButtonNext");
-
+  // Ano anterior.
   btnPrev.onclick = () => {
     year--;
     document.querySelector('.year').innerHTML = year;
   }
-
+  // Proximo ano.
   btnNext.onclick = () => {
     year++;
     document.querySelector('.year').innerHTML = year;
   }
 
-  /*
-  * selectDate(mes)
-  * Seleciona a data que o usuário requisitou
-  */
-
+  /**
+   * Seleciona a data que o usuário requisitou.
+   * Após selecionar a data que o usuário requisitou, a função faz com que essa
+   * data fique ativado no front-end.
+   * @param {String} dateMonth 
+   */
   function selectDate(dateMonth) {
-    // Para converter o mes para numero
+    // Para converter o mes para numero.
     monthSelect = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun','Jul', 
     'Ago', 'Set', 'Out', 'Nov', 'Dez'];
-    // Para trazer o ano que foi selecionado
+    // Para trazer o ano que foi selecionado.
     let yearSelect = document.querySelector('.year').innerHTML;
-    // Seleciona o mes que foi selecionado junto com o ano para deixar a class active
+    // Seleciona o mes que foi selecionado junto com o ano para deixar a class active.
     const table = document.querySelectorAll(".month td");
     for(let i=0; i < table.length; i++){
       if(table[i].innerText === dateMonth) {
@@ -137,7 +117,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
 
-    // Transformando o mês em numero
+    // Transformando o mês em numero.
     let monthNum = null;
     for(let i=0; i<=monthSelect.length; i++) {
       if(monthSelect[i] == dateMonth) {
@@ -145,43 +125,48 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
 
-    // Enviar essa informação para o php para fazer a filtragem do mês e ano
+    // Enviar essa informação para o php para fazer a filtragem do mês e ano.
     return `${monthNum}-${yearSelect}`;
   }
   
-  /*
-  * Seleciona o mês e o ano no calendário e envia esses dados para serem tratados
-  * na função selectDate
-  */
-  
+  /**
+   * Seleciona o mês e o ano no calendário e envia esses dados para serem tratados
+   * na função selectDate.
+   */
   $('.monthSelect').click((e) => {
     $('.monthSelect').removeClass('tdActive');
-    // Caputrando o mes escolhido e enviando para a function
+    // Caputrando o mes escolhido e enviando para a function.
     let monthSelect = e.target.innerHTML;
-    // Recebendo os dados da function
+    // Recebendo os dados da function.
     let date = selectDate(monthSelect);
-    //Envia o ados para o getAjax para enviar via ajax para o php
+    //Envia o ados para o getAjax para enviar via ajax para o php.
     getAjax(date)
-    // Fechando calendario após selecionar o mes e o ano
+    // Fechando calendario após selecionar o mes e o ano.
     $('.appCalendar').slideToggle();
   })
 
 
-  /*
-  * Abrindo o calendário quando for solicitado pelo usuário
-  */
-
+  /**
+   * Abrindo o calendário quando for solicitado pelo usuário.
+   */
   $('.monthSelectActive').click((e) => {
     $('.appCalendar').slideToggle();
   })
 
-
-  /*
-  * Executando métodos quando a página for carregada
+  /**
+  * Executando métodos quando a página for carregada.
   */
   $(document).ready(() => {
-    // Deixando o calendario com display none
+    // Deixando o calendario com display none.
     $('.appCalendar').hide();
+    /**
+     * Escondendo o calendário do header nas demais layouts.
+     */
+    let url = window.location.pathname.split('/');
+    const cauntUrl = url.length - 1;
+    if(url[cauntUrl] != 'app') {
+      $('.headerAppCalendarArea').hide();
+    } 
   })
 
 }, false);
